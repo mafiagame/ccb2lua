aa3f573dcd56093158ca0de8f7e816eec00ce39c
Yan Bin <yanb1985@gmail.com>
2015-05-08 14:26:50 +0800

    new binary of ccb2lua

---
+++ scripts/app/layout/subdir/subButton_layout.lua
@@ -0,0 +1,117 @@
+
+local subdir/subButton_layout = class("subdir/subButton_layout",function()
+    return display.newLayer()
+end)
+
+
+
+
+
+function subdir/subButton_layout:ctor(_owner)
+	 _owner = _owner or self
+	self:setAnchorPoint(cc.p(0.5,0.5))
+	self:setContentSize(cc.size(display.width * 1.0, display.height * 1.0))
+	self:ignoreAnchorPointForPosition(true)
+
+	self._title = display.newTTFLabel({
+		text = "Button测试",
+		font = "Helvetica",
+		size = 30,
+	})
+	self._title:setAnchorPoint(cc.p(0.5,0.5))
+	self._title:setPosition(cc.p(self:getContentSize().width * 0.5, self:getContentSize().height * 0.8))
+	self._title:ignoreAnchorPointForPosition(false)
+
+	self:addChild(self._title)
+
+	local ccmenu_2 = display.newNode()
+	ccmenu_2:setAnchorPoint(cc.p(0.5,0.5))
+	ccmenu_2:ignoreAnchorPointForPosition(true)
+
+	self:addChild(ccmenu_2)
+
+	local ccmenuitemimage_3 = cc.ui.UIPushButton.new({
+		normal = "qianjin_1.png",
+		pressed = "qianjin_2.png",
+	})
+	:onButtonClicked(handler(_owner, _owner.onBtnClick))
+	ccmenuitemimage_3:setAnchorPoint(cc.p(0.5,0.5))
+	ccmenuitemimage_3:setPosition(cc.p(96.768, 402.6241))
+	ccmenuitemimage_3:ignoreAnchorPointForPosition(false)
+
+	ccmenu_2:addChild(ccmenuitemimage_3)
+
+	local ccmenuitemimage_4 = cc.ui.UICheckBoxButton.new({
+		on = "CheckBoxButton2On.png",
+		off = "CheckBoxButton2Off.png"
+	})
+	:onButtonClicked(handler(_owner, _owner.onBtnClick))
+	ccmenuitemimage_4:setAnchorPoint(cc.p(0.5,0.5))
+	ccmenuitemimage_4:setPosition(cc.p(356.768, 402.6241))
+	ccmenuitemimage_4:ignoreAnchorPointForPosition(false)
+
+	ccmenu_2:addChild(ccmenuitemimage_4)
+
+	local cclabelttf_5 = display.newTTFLabel({
+		text = "普通两态按钮",
+		font = "Helvetica",
+		size = 30,
+	})
+	cclabelttf_5:setAnchorPoint(cc.p(0.5,0.5))
+	cclabelttf_5:setPosition(cc.p(self:getContentSize().width * 0.0968, self:getContentSize().height * 0.5246))
+	cclabelttf_5:ignoreAnchorPointForPosition(false)
+
+	self:addChild(cclabelttf_5)
+
+	local cclabelttf_6 = display.newTTFLabel({
+		text = "开关按钮",
+		font = "Helvetica",
+		size = 30,
+	})
+	cclabelttf_6:setAnchorPoint(cc.p(0.5,0.5))
+	cclabelttf_6:setPosition(cc.p(self:getContentSize().width * 0.378125, self:getContentSize().height * 0.525))
+	cclabelttf_6:ignoreAnchorPointForPosition(false)
+
+	self:addChild(cclabelttf_6)
+
+	local cccontrolbutton_7 = cc.ui.UIPushButton.new({
+		normal = "Button01.png",
+		pressed = "Button01Pressed.png"
+	},{scale9 = true})
+	:setButtonSize(self:getContentSize().width * 0.2, self:getContentSize().height * 0.08)
+	:onButtonClicked(handler(_owner, _owner.onBtnClick))
+	local label = cc.ui.UILabel.new({
+            UILabelType = 2,
+            font = "Helvetica",
+            text = "按钮",
+            size = 24,
+            color = cc.c3b(255,255,255),
+        })
+	label:setAnchorPoint(cc.p(0.5,0.5))
+	cccontrolbutton_7:setButtonLabel("normal", label)
+	cccontrolbutton_7:setAnchorPoint(cc.p(0.5,0.5))
+	cccontrolbutton_7:setPosition(cc.p(632.4481, 397.44))
+	cccontrolbutton_7:ignoreAnchorPointForPosition(false)
+
+	self:addChild(cccontrolbutton_7)
+
+	local cclabelttf_8 = display.newTTFLabel({
+		text = "Control按钮",
+		font = "Helvetica",
+		size = 30,
+	})
+	cclabelttf_8:setAnchorPoint(cc.p(0.5,0.5))
+	cclabelttf_8:setPosition(cc.p(self:getContentSize().width * 0.659375, self:getContentSize().height * 0.525))
+	cclabelttf_8:ignoreAnchorPointForPosition(false)
+
+	self:addChild(cclabelttf_8)
+
+
+end
+
+function subdir/subButton_layout:onBtnClick(_tag, _sprite)
+	error("You should override this methed: <onBtnClick>")
+end
+
+
+return subdir/subButton_layout
---
+++ scripts/app/layout/subdir_layout.lua
@@ -0,0 +1,50 @@
+
+local subdir_layout = class("subdir_layout",function()
+    return display.newSprite()
+end)
+
+
+
+
+
+function subdir_layout:ctor(_owner)
+	 _owner = _owner or self
+	self:setSpriteFrame(ccbutils.ccb_display_frame("daxingxing_1.png"))
+	self:setAnchorPoint(cc.p(0.5,0.5))
+	self:ignoreAnchorPointForPosition(false)
+
+	self._name = display.newTTFLabel({
+		text = "第一名",
+		font = "Helvetica",
+		size = 20,
+	})
+	self._name:setAnchorPoint(cc.p(0.5,0.5))
+	self._name:setColor(cc.c3b(0,0,0))
+	self._name:setPosition(cc.p(self:getContentSize().width * 0.5, self:getContentSize().height * 0.5))
+	self._name:ignoreAnchorPointForPosition(false)
+
+	self:addChild(self._name)
+
+	self._score = display.newTTFLabel({
+		text = "500",
+		font = "Helvetica",
+		size = 20,
+	})
+	self._score:setAnchorPoint(cc.p(0.5,0.5))
+	self._score:setColor(cc.c3b(0,0,0))
+	self._score:setPosition(cc.p(self:getContentSize().width * 0.5, self:getContentSize().height * 0.34127))
+	self._score:ignoreAnchorPointForPosition(false)
+
+	self:addChild(self._score)
+
+	local ccbfile_3 = require("app.layout.subdir.subButton_layout").new(_owner)
+	ccbfile_3:setPosition(cc.p(-272, -180))
+
+	self:addChild(ccbfile_3)
+
+
+end
+
+
+
+return subdir_layout
