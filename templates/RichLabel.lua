1b9662275f9de69330d41a24dcf30cf341eda60e
justbilt <wangbilt@gmail.com>
2015-07-08 16:06:02 +0800

    add safe condition

--- templates/RichLabel.lua
+++ templates/RichLabel.lua
@@ -6,7 +6,11 @@
 	-- set properties = data.properties
 {{'\t'}}{{define}}{{name}} = RichLabel.new({
 {{'\t'}}{{'\t'}}fontName = "{{properties.fontName}}",
-{{'\t'}}{{'\t'}}fontColor = cc.c3b({{properties.color[0]}},{{properties.color[1]}},{{properties.color[2]}}),
+	-- set var = properties.color
+	-- if var
+{{'\t'}}{{'\t'}}fontColor = cc.c3b({{var[0]}},{{var[1]}},{{var[2]}}),
+		-- set _ = nilProperty(data.properties, "color")
+	-- endif
 {{'\t'}}{{'\t'}}fontSize = {{properties.fontSize[0]}},
 	-- set var = properties.dimensions
 	-- if var[0] != 0 or var[1] != 0
@@ -14,6 +18,5 @@
 	-- endif
 {{'\t'}}})
 {{'\t'}}{{name}}:setString("{{serializeString(properties.string)}}")
-	-- set _ = nilProperty(data.properties, "color")
 {{ CCNode.rennder_base_properties(name,data.properties,parent) }}
 -- endmacro
